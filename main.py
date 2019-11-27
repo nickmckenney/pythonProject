7e73b7aba75db70ddff22b6acce4dadb62294b64
NickMckenney <mckenney2001@gmail.com>
2019-11-27 13:16:22 -0500

    moving everything into classes for "self"

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import date
 
 db = PostgresqlDatabase('flashcards', user='postgres', password='',
                         host='localhost', port=5432)
+db.connect()
 
 
 class BaseModel(Model):
@@ -16,18 +17,26 @@ class Card(BaseModel):
     answer = CharField()
 
 
-def Menu():
-    a = input("C:Create\nR:Read\n")
-    if a == 'C':
-        question = Card(question=input("NEW QUESTION for your card \n"))
-        question.save()
-        print(f"{question.question}")
+class Menu:
+    def __init__(self):
+        a = input("C:Create\nP: Play\n")
+        if(a == 'P'):
+            game()
+        if a == 'C':
+            question = question = input("NEW QUESTION for your card \n")
+            answer = answer = input("NEW answer for your card \n")
+            card = Card(question=question, answer=answer)
+            card.save()
+            print(f"{card.question} and {card.answer}")
+            question = input("Do you want to add more:y/n ")
+            if question == 'y':
+                # menu()
+                print("x")
 
-    else:
-        print(f"{question.question}")
 
+def game():
+    print("Welcome to the game")
 
-db.connect()
 
 db.create_tables([Card])
-db.drop_tables([Card])
+# menu()
